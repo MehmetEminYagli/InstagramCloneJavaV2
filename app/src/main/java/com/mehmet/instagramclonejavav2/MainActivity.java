36c8a4a6fadd9249a33e8a3149f49a42b3c33109
Mehmet Emin Yagli <m.eminyagli@gmail.com>
2022-10-10 21:16:27 +0300

    kullanıcı adı ve şifre doğru ise giriş yapma kısmını yaptım

--- app/src/main/java/com/mehmet/instagramclonejavav2/MainActivity.java
+++ app/src/main/java/com/mehmet/instagramclonejavav2/MainActivity.java
@@ -27,15 +27,40 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void SignInClicked(View view){
+        String Email = binding.EmailText.getText().toString();
+        String Password = binding.passwordText.getText().toString();
+
+        if (Email.equals("") || Password.equals("")){
+            Toast.makeText(this, "Email veya şifre boş olamaz", Toast.LENGTH_SHORT).show();
+        }else {
+            mAuth.signInWithEmailAndPassword(Email,Password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                //giriş başarılı ise şunnu yap
+                Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+                startActivity(intent);
+                finish();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                //giriş başarılı değilse şunu yap
+
+                Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
 
     }
     public void SignUpClicked(View view){
-
-    String Email = binding.EmailText.getText().toString();
-    String Password = binding.passwordText.getText().toString();
+        String Email = binding.EmailText.getText().toString();
+        String Password = binding.passwordText.getText().toString();
 
     if(Email.equals("") || Password.equals("")){
         Toast.makeText(this, "Email veya şifre Boş okamaz", Toast.LENGTH_SHORT).show();
